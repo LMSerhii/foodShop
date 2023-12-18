8adce7ec94c8e58ab8b74a0f9245b5775634e7da
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2023-12-18 10:22:11 +0100

    add img and button

--- src/js/apiServise.js
+++ src/js/apiServise.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import common from './common';
 
 
+
 async function getCategories() {
   try {
     const data = await axios.get(`${common.BASE_URL}/products/categories`);
@@ -14,8 +15,18 @@ async function getCategories() {
 }
 
 
+async function inputKeyword(keyword) {
+    try {
+        const data = await axios.get(`${common.BASE_URL}/products?page=${common.filters.page}&limit=${common.filters.limit}&keyword=${keyword}`);
+        // console.log('inputKeyword:', data.data.results);
+        return data;
+    } catch (error) {
+        console.error('Ошибка при запросе к серверу:', error.message);
+        throw error; 
+    }
+}
 
 
 
 
-export { getCategories };
+export { getCategories, inputKeyword };
--- src/js/common.js
+++ src/js/common.js
@@ -1,4 +1,5 @@
 const BASE_URL = 'https://food-boutique.b.goit.study/api';
+
 let filters = { page: 1, limit: 6 };
 
 
@@ -6,6 +7,7 @@ let filters = { page: 1, limit: 6 };
 
 export default {
     BASE_URL,
+    
     filters,
   };
 
