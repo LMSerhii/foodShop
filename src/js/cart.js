4926f5fccd73b16bb04518b5fcfbbc925cf0f6f0
Dtimewarp <dtimewarp@gmail.com>
2023-12-19 22:45:34 +0200

    add markup for emptyCart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,66 +1,29 @@
 import axios from 'axios';
 
-function createProductCard(product) {
-    const productContainer = document.getElementById('productContainer');
+function emptyCart(product) {
+  const emptyCartContainer = document.querySelector('.empty-cart');
+  document.body.classList.add('empty-cart-background');
 
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+  const img = document.createElement('img');
+  img.src = '../css/images/yellow shopping basket.png';
+  img.alt = 'Жовтий порожній кошик';
 
-    const img = document.createElement('img');
-    img.src = product.img;
-    img.alt = product.name;
+  const paragraph = document.createElement('p');
+  const span = document.createElement('span');
+  span.className = 'span';
+  span.textContent = 'empty....';
+  paragraph.textContent = 'Your basket is ';
+  paragraph.appendChild(span);
+  
+  const paragraph_2 = document.createElement('p');
+  paragraph_2.textContent =
+    'Go to the main page to select your favorite products and add them to the cart.';
 
-    const h3 = document.createElement('h3');
-    h3.textContent = product.name;
+  emptyCartContainer.appendChild(img);
+  emptyCartContainer.appendChild(paragraph);
+  // emptyCartContainer.appendChild(span);
+  emptyCartContainer.appendChild(paragraph_2);
+  
+}
 
-    const p = document.createElement('p');
-    p.textContent = product.description;
-
-    const span = document.createElement('span');
-    span.classList.add('price');
-    span.textContent = `$${product.price}`;
-
-    const button = document.createElement('button');
-    button.textContent = 'Add to Cart';
-
-    productCard.appendChild(img);
-    productCard.appendChild(h3);
-    productCard.appendChild(p);
-    productCard.appendChild(span);
-    productCard.appendChild(button);
-
-    productContainer.appendChild(productCard);
-  }
-
-  function fetchProducts() {
-    const productContainer = document.getElementById('productContainer');
-    const productLimit = 3;
-    productContainer.innerHTML = ''; // Очищаємо контейнер перед новими продуктами
-
-    // Виконуємо запит за допомогою Axios або іншої бібліотеки
-    axios.get('https://food-boutique.b.goit.study/api/products/popular')
-      .then(response => {
-        // Обробляємо відповідь від сервера
-        const products = response.data.slice(0, productLimit);
-        products.forEach(product => {
-          createProductCard(product);
-        });
-
-        // Додаємо клас "scrollable", якщо кількість продуктів більше 3
-        if (products.length > 3) {
-          productContainer.classList.add('scrollable');
-        } else {
-          productContainer.classList.remove('scrollable');
-        }
-      })
-      .catch(error => {
-        // Обробляємо помилку, якщо виникла
-        console.error('Помилка запиту:', error);
-      });
-  }
-
-  // Викликаємо fetchProducts після повного завантаження сторінки
-  document.addEventListener('DOMContentLoaded', function() {
-    const fetchButton = document.getElementById('fetchButton');
-    fetchButton.addEventListener('click', fetchProducts);
-  });
+emptyCart();
