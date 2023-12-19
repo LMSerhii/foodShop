98d2652c04106d83c170b6d49aadfaaf530dceb6
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-19 10:37:27 +0200

    add file

---
+++ src/js/popular.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://food-boutique.b.goit.study/api/products';
+const END_POINT = '/popular';
+
+const queryParam = {
+  page: page,
+  limit: 6,
+};
+
+async function render() {}
