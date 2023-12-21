8946ffaa746edd1612e5b607074099f158059374
JuliaLuk <julia1lukasiewicz@gmail.com>
2023-12-21 15:01:04 -0600

    start with discount

---
+++ src/js/dicsount_products.js
@@ -0,0 +1,41 @@
+import { getDiscount } from './api_service';
+const dicsProd = document.querySelector('.discount-product-list');
+// fetch data
+async function dataDiscountProd() {
+  const data = await getDiscount();
+  console.log(data);
+  const markup = createDiscountMarkup(data);
+  dicsProd.innerHTML = markup;
+}
+dataDiscountProd();
+
+// add to ul
+
+function createDiscountMarkup(array) {
+  return array
+    .map(({ img, name, price, _id, is10PercentOff }) => {
+      if (is10PercentOff) {
+        return `
+        <li class="discount-product-element">
+        <div class="discount-product-img-wrapper">
+        <img class="discount-product-img" src="${img}" alt="${name}">
+        </div>
+        
+        <div class="discount-product-descr">
+         <div class="discount-product-name-wrapper">
+           <h3 class="discount-product-name">${name}</h3>
+          </div>
+         <div class="discount-product-price-wrapper">
+          <p class="discount-product-price">$${price}</p>
+          <button class="discount-product-btn" type="button">add</button>
+          </div>
+        </div>
+        </li>`;
+      }
+    })
+    .join('');
+}
+
+//  <svg class="" width="" height="">
+//    <use href="${svg_sprite}#cart"></use>
+//  </svg>
