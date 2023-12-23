c68797ee880e17c6ea54e7673f50ae98f487752e
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-23 17:41:17 +0200

    Update popular.js

--- src/js/popular.js
+++ src/js/popular.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { getPopular } from './api_service';
 
 import { getDataId } from './api_service';
+import svg_sprite from '../img/sprite.svg';
 // import { save, load } from './storage';
 // import { addToCart } from './helpers/addToCart';
 // import { productMarkup, notFoundMarkup } from './markupFunctions';
@@ -36,8 +37,9 @@ function createMarkup(arrey) {
     return;
   }
   return arrey
-    .map(({ _id, name, img, category, size, popularity }) => {
-      return `<li class="popular-card" data-id=${_id}>
+    .map(({ _id, name, img, category, size, popularity, is10PercentOff }) => {
+      if (!is10PercentOff) {
+        return `<li class="popular-card" data-id=${_id}>
                     <div class="popular-box">
                       <div class="popular-card-img-wrap">
                           <img src="${img}" alt="${name}" width="56" height="56" loading="lazy"/>
@@ -60,11 +62,44 @@ function createMarkup(arrey) {
                       
                       <button class="card-btn" type="button">
                           <svg class="icon" width="18" height="18">
-                              <use href="./img/sprite.svg#cart"></use>
+                              <use href="${svg_sprite}#cart"></use>
                           </svg>
                       </button>
                     </div>
                 </li>`;
+      } else {
+        return `<li class="popular-card" data-id=${_id}>
+                    <div class="popular-box">
+                      <div class="popular-card-img-wrap">
+                          <img src="${img}" alt="${name}" width="56" height="56" loading="lazy"/>
+                      </div>
+                      <div class="card-info-box">
+                      
+                        <h3 class="popular-name">${name}</h3>
+                      
+                      <ul class="popular-info-list">
+                          <li class="info-style">Category:
+                          <span>${category.replace('_', ' ')}</span></li>
+                          <div class="popular-info-wraper">
+                          <li class="info-style">Size: <span>${size}</span></li>
+                          <li class="info-style">Popularity: <span>${popularity}</span></li>
+                      </ul>
+                      </div>
+                    </div>
+
+                    <div class="card-bottom">
+                      
+                      <button class="card-btn" type="button">
+                          <svg class="icon" width="18" height="18">
+                              <use href="${svg_sprite}#cart"></use>
+                          </svg>
+                      </button>
+                      <svg class="discount-icon" width="60" height="60">
+                <use href="${svg_sprite}#discount"></use>
+              </svg>
+                    </div>
+                </li>`;
+      }
     })
     .join('');
 }
