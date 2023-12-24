995d960c5927cbb93eba507056a66f19e01d3f1f
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-24 12:06:48 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,3 +9,14 @@ import './js/popular';
 // console.log(storage_query);
 
 // renderProducts(storage_query);
+
+// import { renderProducts } from './js/products';
+// import { save, load } from './js/storage';
+// import { common } from './js/common';
+
+// save(common.LOCAL_QUERY_KEY, common.INIT_QUERY);
+
+// const storage_query = load(common.LOCAL_QUERY_KEY) ?? [];
+// console.log(storage_query);
+
+// renderProducts(storage_query);
