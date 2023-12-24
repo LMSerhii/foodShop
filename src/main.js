e36fdbe91de27ec5b166560c602245f084708b63
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-24 10:01:24 +0200

    update container

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './js/popular';
+// import { renderProducts } from './js/products';
+// import { save, load } from './js/storage';
+// import { common } from './js/common';
+
+// save(common.LOCAL_QUERY_KEY, common.INIT_QUERY);
+
+// const storage_query = load(common.LOCAL_QUERY_KEY) ?? [];
+// console.log(storage_query);
+
+// renderProducts(storage_query);
