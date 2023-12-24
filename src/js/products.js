246a9ff94b025543e1699a093a341bf67cfac3ff
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-24 20:49:01 +0200

    update

--- src/js/products.js
+++ src/js/products.js
@@ -36,4 +36,80 @@ const onProductList = evt => {
 
 refs.productList.addEventListener('click', onProductList);
 
+// Отримання даних від сервера (ваш власний метод)
+import * as tui from 'tui-pagination'; // або імпорт { Pagination } from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
+// Решта вашого коду
+
+// import $ from 'jquery';
+// tui.use($, window.jQuery);
+
+async function fetchPage({ page, itemsPerPage }) {
+  try {
+    const response = await axios({
+      url: `${common.BASE_URL}/products`,
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      params: { page, itemsPerPage },
+    });
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.getElementById('pagination');
+  const itemsPerPage = 10;
+  let currentPage = 1;
+
+  function updatePage(pageData) {
+    const totalItems = pageData.totalItems;
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    const pagination = new tui.Pagination(container, {
+      totalItems: totalItems,
+      itemsPerPage: itemsPerPage,
+      visiblePages: 5,
+      page: currentPage,
+      centerAlign: false,
+      firstItemClassName: 'tui-first-child',
+      lastItemClassName: 'tui-last-child',
+      template: {
+        page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+        currentPage:
+          '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+        moveButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</a>',
+        disabledMoveButton:
+          '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-ico-{{type}}">{{type}}</span>' +
+          '</span>',
+        moreButton:
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+          '<span class="tui-ico-ellip">...</span>' +
+          '</a>',
+      },
+    });
+
+    pagination.on('beforeMove', event => {
+      const newPage = event.page;
+      if (newPage !== currentPage) {
+        currentPage = newPage;
+        fetchPage({ page: currentPage, itemsPerPage: itemsPerPage }).then(
+          updatePage
+        );
+      }
+      event.stop();
+    });
+  }
+
+  fetchPage({ page: currentPage, itemsPerPage: itemsPerPage }).then(updatePage);
+});
+
 export { renderProducts };
