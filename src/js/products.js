f892c4d1ff23e783c64a1eccd622ccbea545aec7
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-24 22:36:02 +0200

    Update products.js

--- src/js/products.js
+++ src/js/products.js
@@ -39,27 +39,69 @@ const onProductList = evt => {
 refs.productList.addEventListener('click', onProductList);
 
 // Отримання даних від сервера (ваш власний метод)
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+
+// const container = document.getElementById('pagination-container');
+// const pagination = new Pagination(container, {
+//   totalItems: 500,
+// });
+
+// let itemsPerPage = 10;
+// let currentPage = 1;
+
+// async function fetchPage({ page, perPage }) {
+//   try {
+//     const response = await axios({
+//       url: `${common.BASE_URL}/products`,
+//       method: 'GET',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       params: { page, perPage },
+//     });
+//     if (response.status === 200) {
+//       return response.data;
+//     } else {
+//       throw new Error(`Request failed with status ${response.status}`);
+//     }
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     throw error;
+//   }
+// }
+
+// function updatePage(pageData) {
+//   // Обробляйте отримані дані та оновлюйте сторінку
+//   console.log('Received page data:', pageData);
+// }
+
+// pagination.on('beforeMove', event => {
+//   const newPage = event.page;
+//   if (newPage !== currentPage) {
+//     currentPage = newPage;
+
+//     fetchPage({ page: currentPage, perPage: itemsPerPage }).then(updatePage);
+
+//     // Використовуйте return false для відміни стандартної обробки події
+//     return false;
+//   }
+// });
+
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 
 const container = document.getElementById('pagination-container');
-const pagination = new Pagination(container, {
-  totalItems: 500,
-});
-
 let itemsPerPage = 10;
 let currentPage = 1;
 
-async function fetchPage({ page, perPage }) {
+// Отримання даних від сервера (ваш власний метод)
+async function fetchPageData(page) {
   try {
-    const response = await axios({
-      url: `${common.BASE_URL}/products`,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      params: { page, perPage },
+    const response = await axios.get(`${common.BASE_URL}/products`, {
+      params: { page, itemsPerPage },
     });
+
     if (response.status === 200) {
       return response.data;
     } else {
@@ -72,16 +114,40 @@ async function fetchPage({ page, perPage }) {
 }
 
 function updatePage(pageData) {
-  // Обробляйте отримані дані та оновлюйте сторінку
+  // Оновлення сторінки з отриманими даними
   console.log('Received page data:', pageData);
 }
 
-pagination.on('beforeMove', event => {
+const pagination = new Pagination(container, {
+  totalItems: 500, // Загальна кількість елементів на сервері
+  itemsPerPage: itemsPerPage,
+  visiblePages: 5,
+  page: currentPage,
+  centerAlign: false,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton: '<a href="#" class="tui-page-btn tui-{{type}}">{{type}}</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">{{type}}</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+});
+
+pagination.on('beforeMove', async event => {
   const newPage = event.page;
+
   if (newPage !== currentPage) {
     currentPage = newPage;
 
-    fetchPage({ page: currentPage, perPage: itemsPerPage }).then(updatePage);
+    const pageData = await fetchPageData(currentPage);
+    updatePage(pageData);
 
     // Використовуйте return false для відміни стандартної обробки події
     return false;
