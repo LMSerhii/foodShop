60a7e6afd18676f20d19a2e7856d5e1e3a919ca8
RayChikS <vatoresp@gmail.com>
2023-12-25 20:06:45 +0300

    delete-js
    удалил лишний js

--- src/js/header.js
+++
@@ -1,8 +0,0 @@
-// временная заглушка для счетчика продуктов в корзине
-const itemsInCart = 0;
-
-const cartTextElement = document.querySelector('.cart-text');
-
-const counter = cartTextElement.querySelector('#cartCount');
-
-counter.textContent = itemsInCart;
