abea7a51e488c00733d3c85171449e346f33a8b9
cefariron <adidaswww17@gmail.com>
2023-12-26 08:12:08 +0200

    Pagination-2 first pull request (mobile version)

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -6,6 +6,60 @@ import { common } from './common';
 import { refs } from './refs';
 import { productMarkup } from './markupFunctions';
 
+
+// Отслеживаю вьюпорт
+let visPagesPag;
+const mediaQuery = window.matchMedia("(min-width: 768px)");
+
+const handleMediaChange = (event) => {
+  if (event.matches) {
+    visPagesPag = 4;
+    loadPaginationDataOnChangeViewport()
+  } else {
+    visPagesPag = 2;
+    loadPaginationDataOnChangeViewport()
+  }
+};
+
+handleMediaChange(mediaQuery);
+mediaQuery.addEventListener("change", handleMediaChange);
+
+//Функция создания и управления пагинацией при изменении вьюпорта
+function loadPaginationDataOnChangeViewport() {
+
+  const pagination = new Pagination(refs.paginationContainer, {
+    page: load('pages').page,
+    totalItems: load('pages').perPage * load('pages').totalPages,
+    itemsPerPage: load('pages').perPage,
+    visiblePages: visPagesPag,
+    centerAlign: true,
+    firstItemClassName: 'tui-first-child',
+    lastItemClassName: 'tui-last-child',
+  });
+
+  pagination.on('beforeMove', function (eventData) {
+    const pagesData = JSON.parse(localStorage.getItem('pages'));
+    pagesData.page = eventData.page;
+    localStorage.setItem('pages', JSON.stringify(pagesData));
+    const storage_query = JSON.parse(localStorage.getItem('pages'));
+
+    const currentQuery = load(common.LOCAL_QUERY_KEY);
+    currentQuery.page = storage_query.page;
+    save(common.LOCAL_QUERY_KEY, currentQuery);
+    const query = load(common.LOCAL_QUERY_KEY);
+
+    renderProductsOnPagination(query);
+  });
+
+  const data = JSON.parse(localStorage.getItem('pages'));
+  if (data.totalPages <= 1) {
+    refs.paginationContainer.style.display = "none";
+  } else {
+    refs.paginationContainer.style.display = "block";
+  }
+}
+
+
 //Функция создания и управления пагинацией, запускается в renderProducts() и в renderProductsSort().
 export function loadPaginationData() {
 
@@ -13,7 +67,7 @@ export function loadPaginationData() {
     page: common.INIT_QUERY.page,
     totalItems: load('pages').perPage * load('pages').totalPages,
     itemsPerPage: load('pages').perPage,
-    visiblePages: 2,
+    visiblePages: visPagesPag,
     centerAlign: true,
     firstItemClassName: 'tui-first-child',
     lastItemClassName: 'tui-last-child',
