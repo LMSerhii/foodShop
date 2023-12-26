45d996a125a9712b59ca4c5a86565bc34bff6660
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-26 15:44:42 +0200

    update

--- src/js/helpers/openModal.js
+++ src/js/helpers/openModal.js
@@ -1,155 +1,28 @@
 import { getDataId } from '../api_service';
 import { refs } from '../refs';
 
-const modal = {
-  openModal: document.querySelector('[data-modal-open]'),
-  closeModal: document.querySelector('.modal__close'),
-  dataModal: document.querySelector('[data-modal]'),
-};
-
-// modal.closeModal.addEventListener('click', handleClick);
-modal.openModal.addEventListener('click', handleClick);
-modal.dataModal.addEventListener('click', handleBackdropClick);
-window.addEventListener('keydown', handleKeyPress);
-
-function handleClick() {
-  modal.dataModal.classList.toggle('is-hidden');
-}
-
-function handleBackdropClick(event) {
-  if (event.target === modal.dataModal) {
-    modal.dataModal.classList.add('is-hidden');
-  }
-}
-function handleKeyPress(event) {
-  if (event.key === 'Escape') {
-    modal.dataModal.classList.add('is-hidden');
-  }
-}
-
-const openModal = async evt => {
-  const { id } = evt.target.closest('.js-card').dataset;
-
-  try {
-    const data = await getDataId(id);
-
-    if (response === true) {
-      const successContent = successModal();
-      openModal(successContent);
-    } else if (response === false) {
-      const errorContent = errorModal();
-      openModal(errorContent);
-    } else {
-      console.log('Не виконано', response);
-    }
-  } catch (err) {
-    console.error(err);
-  }
-
-  save(common.PAGES, {
-    page: data.page,
-    perPage: data.perPage,
-    totalPages: data.totalPages,
-  });
-  modal.dataModal.innerHTML = content;
-  modal.dataModal.classList.remove('is-hidden');
-};
-
-// function modalProductMarkup(arrey) {
-//   return arrey.map(
-//     ({ _id, name, img, category, price, size, popularity, desc }) => {
-//       `<li class="card js-card" data-id=${_id}>
-//             <div class="card-top">
-
-//               <div class="card-img-wrapper">
-//                   <img src="${img}" alt="${name}" width="140" height="140" loading="lazy"/>
-//               </div>
-
-//               <h3 class="card-product-name">${name}</h3>
-
-//               <ul class="card-prodcuts-list">
-//                   <li class="card-prodcuts-item">Category:
-//                     <span>${category.replace('_', ' ')}</span>
-//                   </li>
-//                   <li class="card-prodcuts-item">Size:
-//                     <span>${size}</span>
-//                   </li>
-//                   <li class="card-prodcuts-item">Popularity:
-//                     <span>${popularity}</span>
-//                   </li>
-//                   <li class ="describtion">
-//                    <p class="text-desc">${desc}</p>
-//                   </li>
-//               </ul>
-
-//             </div>
-
-//             <div class="card-bottom">
-//               <p class="card-producs-price">$${price}</p>
-
-//               <button class="card-btn js-cart" type="button">
-
-//                   <svg class="card-btn-icon" width="18" height="18">
-//                       <use href="${svg_sprite}#cart"></use>
-//                   </svg>
-
-//               </button>
-
-//             </div>
-//         </li>`;
-//     }
-//   );
+// Функція для відображення модального вікна
+// function showModal(modalId) {
+//   var modal = document.getElementById(modalId);
+//   modal.style.display = 'block';
 // }
 
-function successModal() {
-  return `<div class="modal-box">
-  <button class="modal-close" type="button" data-modal-close>
-    <svg class="modal-icon" width="22" height="22">
-      <use href="./img/sprite.svg#close"></use>
-    </svg>
-  </button>
-  <div class="modal-text-box">
-      <p class="subscription-thanks">
-        Thanks for subscribing for
-        <span class="subscription-accent">new</span> products
-      </p>
-      <p class="subscription-promise">
-        We promise you organic and high-quality products that will meet your
-        expectations. Please stay with us and we promise you many pleasant
-        surprises.
-      </p>
-    <img
-      class="modal-image"
-      srcset="../img/subscribing@1x.png 1x, ../img/subscribing@2x.png 2x"
-      src="../img/subscribing@1x.png"
-      alt="basket of fresh vegetables"
-      width="490"
-    />
-  </div>
-</div>
-`;
-}
+// // Функція для закриття модального вікна
+// function closeModal(modalId) {
+//   var modal = document.getElementById(modalId);
+//   modal.style.display = 'none';
+// }
 
-function errorModal() {
-  return `<div class="modal__box">
-  <button class="modal__close" type="button" data-modal-close>
-    <svg class="modal__icon" width="22" height="22">
-      <use href="./img/sprite.svg#close"></use>
-    </svg>
-  </button>
-  <div class="modal-text-box">
-  <p class="subscription-thanks">
-  This <span class="subscription-accent">email address</span> has already been entered
-  </p>
-  <p class="subscription-promise">
-  You have already subscribed to our new products. Watch for offers at the mailing address.
-  </p>`;
-}
-
-export { openModal };
-
-// // import {onSendBtn} from './footer';
-// import { refs } from './refs';
+// // Закриття модального вікна при кліку поза ним
+// window.onclick = function (event) {
+//   var modals = document.getElementsByClassName('modal');
+//   for (var i = 0; i < modals.length; i++) {
+//     var modal = modals[i];
+//     if (event.target == modal) {
+//       modal.style.display = 'none';
+//     }
+//   }
+// };
 
 // const modal = {
 //   openModal: document.querySelector('[data-modal-open]'),
@@ -177,74 +50,82 @@ export { openModal };
 //   }
 // }
 
-// // async function createSubscription() {
-// //   const isAlreadySubscribed = true;
-// //   return isAlreadySubscribed;
-// // }
+// const openModal = async evt => {
+//   const { id } = evt.target.closest('.js-card').dataset;
 
-// async function createMarkup() {
 //   try {
-//     const response = await createSubscription();
-//     if (response === true) {
-//       const successContent = successModal();
+//     const data = await getDataId(id);
+
+//     if (data === true) {
+//       const successContent = successModal(data);
 //       openModal(successContent);
-//     } else if (response === false) {
-//       const errorContent = errorModal();
-//       openModal(errorContent);
-//     } else {
-//       console.log('Не виконано', response);
 //     }
+
+//     save(common.PAGES, {
+//       page: data.page,
+//       perPage: data.perPage,
+//       totalPages: data.totalPages,
+//     });
+//     modal.dataModal.innerHTML = successContent;
+//     modal.dataModal.classList.remove('is-hidden');
 //   } catch (err) {
 //     console.error(err);
 //   }
-// }
-// function openModal(content) {
-//   modal.dataModal.innerHTML = content;
-//   modal.dataModal.classList.remove('is-hidden');
-// }
-// createMarkup();
+// };
 
-// function successModal() {
-//   return `<div class="modal-box">
+// function successModal(arrey) {
+//   return arrey.map(
+//     ({ _id, name, img, category, price, size, popularity, desc }) => {
+//       return `<div class="modal-box">
 //   <button class="modal-close" type="button" data-modal-close>
 //     <svg class="modal-icon" width="22" height="22">
 //       <use href="./img/sprite.svg#close"></use>
 //     </svg>
 //   </button>
-//   <div class="modal-text-box">
-//       <p class="subscription-thanks">
-//         Thanks for subscribing for
-//         <span class="subscription-accent">new</span> products
-//       </p>
-//       <p class="subscription-promise">
-//         We promise you organic and high-quality products that will meet your
-//         expectations. Please stay with us and we promise you many pleasant
-//         surprises.
-//       </p>
-//     <img
-//       class="modal-image"
-//       srcset="../img/subscribing@1x.png 1x, ../img/subscribing@2x.png 2x"
-//       src="../img/subscribing@1x.png"
-//       alt="basket of fresh vegetables"
-//       width="490"
-//     />
 //   </div>
-// </div>
-// `;
+//   <div class="modal-text-box">
+//   <li class="card js-card" data-id=${_id}>
+//             <div class="card-top">
+
+//               <div class="card-img-wrapper">
+//                   <img class="img-modal" src="${img}" alt="${name}" width="300" height="300" loading="lazy"/>
+//               </div>
+
+//               <h3 class="card-product-name">${name}</h3>
+
+//               <ul class="card-prodcuts-list">
+//                   <li class="card-prodcuts-item">Category:
+//                     <span>${category.replace('_', ' ')}</span>
+//                   </li>
+//                   <li class="card-prodcuts-item">Size:
+//                     <span>${size}</span>
+//                   </li>
+//                   <li class="card-prodcuts-item">Popularity:
+//                     <span>${popularity}</span>
+//                   </li>
+//                   <li class ="describtion">
+//                     <p class="text-desc">${desc}</p>
+//                   </li>
+//               </ul>
+
+//             </div>
+
+//             <div class="card-bottom">
+//               <p class="card-producs-price">$${price}</p>
+
+//               <button class="card-btn js-cart" type="button">
+
+//                   <svg class="card-btn-icon" width="18" height="18">
+//                       <use href="${svg_sprite}#cart"></use>
+//                   </svg>
+
+//               </button>
+
+//             </div>
+//               </li>
+//               </div>`;
+//     }
+//   );
 // }
 
-// function errorModal() {
-//   return `<div class="modal__box">
-//   <button class="modal__close" type="button" data-modal-close>
-//     <svg class="modal__icon" width="22" height="22">
-//       <use href="./img/sprite.svg#close"></use>
-//     </svg>
-//   </button>
-//   <div class="modal-text-box">
-//   <p class="subscription-thanks">
-//   This <span class="subscription-accent">email address</span> has already been entered
-//   </p>
-//   <p class="subscription-promise">
-//   You have already subscribed to our new products. Watch for offers at the mailing address.
-//   </p>`;
-// }
+export { openModal };
--- src/js/products.js
+++ src/js/products.js
@@ -1,12 +1,12 @@
 import { common } from './common';
 import { refs } from './refs';
 import { getData } from './api_service';
+import { getDataId } from './api_service';
 import { save, load } from './storage';
 import { addToCart } from './helpers/addToCart';
 import { productMarkup, notFoundMarkup } from './markupFunctions';
 
 const renderProducts = async query => {
-
   const data = await getData(query);
 
   if (!data.results.length) {
@@ -31,11 +31,159 @@ const onProductList = evt => {
   }
 
   if (evt.target.classList.contains('js-info')) {
-    openModal(evt);
+    displayProductDetails(evt);
   }
 };
 
 refs.productList.addEventListener('click', onProductList);
 
-export { renderProducts };
+// function showModal(modalId) {
+//   var modal = document.getElementById(modalId);
+//   modal.style.display = 'block';
+// }
 
+// // Функція для закриття модального вікна
+// function closeModal(modalId) {
+//   var modal = document.getElementById(modalId);
+//   modal.style.display = 'none';
+// }
+
+// // Закриття модального вікна при кліку поза ним
+// window.onclick = function (event) {
+//   var modals = document.getElementsByClassName('modal');
+//   for (var i = 0; i < modals.length; i++) {
+//     var modal = modals[i];
+//     if (event.target == modal) {
+//       modal.style.display = 'none';
+//     }
+//   }
+// };
+
+document.addEventListener('DOMContentLoaded', function () {
+  const modal = {
+    openModal: document.querySelector('[data-modal-open]'),
+    closeModal: document.querySelector('.modal__close'),
+    dataModal: document.getElementById('productModal'), // Замініть 'productModal' на унікальний id
+  };
+
+  console.log('modal.dataModal:', modal.dataModal);
+
+  // Перевірка наявності елементів перед додаванням обробників подій
+  if (modal.openModal) {
+    modal.openModal.addEventListener('click', handleOpenModal);
+  }
+
+  if (modal.dataModal) {
+    modal.dataModal.addEventListener('click', handleBackdropClick);
+  }
+
+  window.addEventListener('keydown', handleKeyPress);
+
+  function handleOpenModal() {
+    if (modal.dataModal) {
+      modal.dataModal.classList.toggle('is-hidden');
+    }
+  }
+
+  function handleBackdropClick(event) {
+    if (
+      event.target === modal.dataModal ||
+      event.target.closest('[data-modal-close]')
+    ) {
+      modal.dataModal.classList.add('is-hidden');
+      modal.dataModal.removeEventListener('click', handleBackdropClick);
+    }
+  }
+
+  function handleKeyPress(event) {
+    if (event.key === 'Escape' && modal.dataModal) {
+      modal.dataModal.classList.add('is-hidden');
+    }
+  }
+
+  const displayProductDetails = async evt => {
+    console.log(
+      'Clicked on product card:',
+      evt.target.closest('.js-info').dataset
+    );
+    const { id } = evt.target.closest('.js-info').dataset;
+
+    try {
+      const data = await getDataId(id);
+      console.log('Data from getDataId:', data);
+
+      if (data) {
+        const successContent = successModal(data);
+        console.log('Success content:', successContent);
+
+        // Видалено зайвий виклик функції
+        modal.dataModal.innerHTML = successContent;
+        modal.dataModal.classList.remove('is-hidden');
+        modal.dataModal.addEventListener('click', handleBackdropClick);
+      }
+
+      save(common.PAGES, {
+        page: data.page,
+        perPage: data.perPage,
+        totalPages: data.totalPages,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  function successModal(array) {
+    return array
+      .map(({ _id, name, img, category, price, size, popularity, desc }) => {
+        return `
+        <button class="modal-close" type="button" data-modal-close>
+          <svg class="modal-icon" width="22" height="22">
+            <use href="./img/sprite.svg#close"></use>
+          </svg>
+        </button>
+
+      <div>
+        <li class="card js-info" data-id=${_id}>
+          <div class="card-top">
+            <div class="card-img-wrapper">
+              <img class="img-modal" src="${img}" alt="${name}" width="300" height="300" loading="lazy"/>
+            </div>
+            <h3 class="card-product-name">${name}</h3>
+            <ul class="card-prodcuts-list">
+              <li class="card-prodcuts-item">Category:
+                <span>${category.replace('_', ' ')}</span>
+              </li>
+              <li class="card-prodcuts-item">Size:
+                <span>${size}</span>
+              </li>
+              <li class="card-prodcuts-item">Popularity:
+                <span>${popularity}</span>
+              </li>
+              <li class ="describtion">
+                <p class="text-desc">${desc}</p>
+              </li>
+            </ul>
+          </div>
+          <div class="card-bottom">
+            <p class="card-producs-price">$${price}</p>
+            <button class="card-btn js-info" type="button">
+              <svg class="card-btn-icon" width="18" height="18">
+                <use href="${svg_sprite}#cart"></use>
+              </svg>
+            </button>
+          </div>
+        </li>
+      </div>`;
+      })
+      .join(''); // Об'єднати рядки масиву в один рядок
+  }
+
+  // Додавання обробника подій на елемент з data-modal-open=""
+  if (modal.dataModal) {
+    modal.dataModal.addEventListener('click', handleOpenModal);
+  }
+});
+
+// export { displayProductDetails };
+
+export { renderProducts };
