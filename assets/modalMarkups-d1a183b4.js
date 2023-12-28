7cb39c375187a2913a0e7b641b88d560cb311628
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-12-28 14:45:38 +0000

    Deploying to gh-pages from @ LMSerhii/foodShop@4ff94623013417ddcc675bbc590cccca608f0d32 🚀

--- assets/modalMarkups-d1a183b4.js
+++ assets/modalMarkups-d1a183b4.js
@@ -430,9 +430,11 @@ import{a as l,b as m}from"./vendor-85c19974.js";(function(){const s=document.cre
                         <span class="list-variables">${d}</span>
                       </li>
                     </ul>
+                    <div class="scroll">
                     <p class="modal-product-desc">
                       ${s}
                     </p>
+                    </div>
                   </div>
                 </div>
 
@@ -458,4 +460,4 @@ import{a as l,b as m}from"./vendor-85c19974.js";(function(){const s=document.cre
           </div>
 
 `;export{O as a,I as b,n as c,w as d,v as e,c as f,q as g,E as h,S as i,F as j,x as k,$ as l,M as m,j as n,B as o,L as p,_ as q,g as r,f as s,T as t,k as u,P as v,C as w,A as x,R as y,z};
-//# sourceMappingURL=modalMarkups-b5407a0d.js.map
+//# sourceMappingURL=modalMarkups-d1a183b4.js.map
